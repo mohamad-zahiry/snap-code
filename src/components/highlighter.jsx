3cce6d35693ed092114b7746e3021f12a75afc23
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-11-29 22:28:21 +0330

    Get source code form user

--- src/components/highlighter.jsx
+++ src/components/highlighter.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
@@ -7,19 +7,14 @@ import AllStyls from "../styles/hljs"
 
 let AllStylesKeys = Object.keys(AllStyls)
 
-const codeString =
-    `import math
-import sys
-
-def root(x):
-print(math.sqrt(x))
-
-if __name__ == "__main__":
-root(sys.argvp=[1])
-`
 
 const Highlighter = () => {
     const [style, setStyle] = useState(dracula)
+    const [codeString, setCodeString] = useState("")
+
+    const handleCodeString = (e) => {
+        setCodeString(e.target.value)
+    }
 
     const style_changer = (_style) => {
         const style_select = document.getElementById("style")
@@ -29,7 +24,14 @@ const Highlighter = () => {
 
     return (
         <>
-            <div style={{ width: "450px" }}>
+            <div>
+                <textarea
+                    name="code_string" id="code_string"
+                    cols="30" rows="10" value={codeString}
+                    onChange={handleCodeString}
+                ></textarea>
+            </div>
+            <div>
                 <SyntaxHighlighter language="python" style={style} showLineNumbers={true} >
                     {codeString}
                 </SyntaxHighlighter>
