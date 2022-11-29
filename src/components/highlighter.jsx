87d5f264429153737041b6215bd744a680914cfb
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-11-29 22:34:46 +0330

    Add suport for all programing languages

--- src/components/highlighter.jsx
+++ src/components/highlighter.jsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import languges from 'react-syntax-highlighter/src/languages/hljs/supported-languages'
 
 import AllStyls from "../styles/hljs"
 
@@ -11,15 +12,20 @@ let AllStylesKeys = Object.keys(AllStyls)
 const Highlighter = () => {
     const [style, setStyle] = useState(dracula)
     const [codeString, setCodeString] = useState("")
+    const [language, setLanguage] = useState("")
 
     const handleCodeString = (e) => {
         setCodeString(e.target.value)
     }
 
     const style_changer = (_style) => {
-        const style_select = document.getElementById("style")
-        style_select.value = _style
+        // const style_select = document.getElementById("style")
+        // style_select.value = _style
         setStyle(AllStyls[_style])
+    }
+
+    const language_changer = (lang) => {
+        setLanguage(lang)
     }
 
     return (
@@ -32,7 +38,7 @@ const Highlighter = () => {
                 ></textarea>
             </div>
             <div>
-                <SyntaxHighlighter language="python" style={style} showLineNumbers={true} >
+                <SyntaxHighlighter language={language} style={style} showLineNumbers={true} >
                     {codeString}
                 </SyntaxHighlighter>
             </div>
@@ -40,6 +46,10 @@ const Highlighter = () => {
             <div>
                 <select id="style" onChange={(e) => { style_changer(e.target.value) }}>
                     {AllStylesKeys.map((l) => <option key={l} value={l}>{l}</option>)}
+                </select>
+
+                <select id="language" onChange={(e) => { language_changer(e.target.value) }}>
+                    {languges.map((l) => <option key={l} value={l}>{l}</option>)}
                 </select>
             </div>
 
